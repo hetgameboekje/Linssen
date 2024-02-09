622157ff8f0d1724435d9c9e739c64de39f94cd5
HetGameBoekje <bergthaler.timo@gmail.com>
2024-02-09 14:12:23 +0100

    added gps to page

--- front-end/php/home.php
+++ front-end/php/home.php
@@ -1,17 +1,11 @@
-<?php 
-require "../../backend/php/session_manager.php";
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
-
-<link rel="stylesheet" href="../css/main.css">
-<link rel="stylesheet" href="../css/home.css">
-
+    <link rel="stylesheet" href="../css/main.css">
+    <link rel="stylesheet" href="../css/home.css">
 </head>
 <body>
 
@@ -26,11 +20,20 @@ require "../../backend/php/session_manager.php";
 </div>
 
 <div id="MeldenDiv">
-<form action="/backend/panicPress/panicPress.php" method="post">
-    <a href="../php/incidenteninfo.php">
-        <img src="../Images/MeldenBtnAccent.png" id="MeldenBtnAccent">
-    <button id="MeldenBtn"> MELDEN </button></a>
+    <form action="/backend/panicPress/panicPress.php" method="post">
+        <a href="../php/incidenteninfo.php">
+            <img src="../Images/MeldenBtnAccent.png" id="MeldenBtnAccent">
+            <button id="MeldenBtn" onclick="gps_location()"> MELDEN </button>
+        </a>
+    </form>
 </div>
+
+<!-- Display latitude and longitude -->
+Latitude: <span id="latitude"></span><br>
+Longitude: <span id="longitude"></span>
+
+<script src="../js/gps.js">
+</script>
 
 </body>
 </html>
