df3d1d17e2afce1976192e621c01de71a112b491
HetGameBoekje <bergthaler.timo@gmail.com>
2024-02-09 23:16:52 +0100

    gps

--- front-end/js/gps.js
+++ front-end/js/gps.js
@@ -1,57 +1,35 @@
-function gps_location () {
-    // Check if the browser supports Geolocation
-    alert("test");
+function e() {
     if ("geolocation" in navigator) {
         navigator.geolocation.getCurrentPosition(
-            function(position) {
-                var latitude = position.coords.latitude;
-                var longitude = position.coords.longitude;
-
-                // Update the HTML elements with the obtained coordinates
-                document.getElementById("latitude").innerText = latitude;
-                document.getElementById("longitude").innerText = longitude;
-
-                // Create a JSON object with the coordinates
-                var locationData = {
-                    latitude: latitude,
-                    longitude: longitude
-                };
-
-                // Send the location data to your backend
-                sendLocationData(locationData);
-            }, 
-            function(error) {
-                console.error("Error getting location:", error.message);
+            function (n) {
+                var t = n.coords.latitude,
+                    o = n.coords.longitude;
+                document.getElementById("latitude").innerText = t,
+                    document.getElementById("longitude").innerText = o;
+                var a = { latitude: t, longitude: o };
+                fetch("./incidenteninfo.php", {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json"
+                    },
+                    body: JSON.stringify(a)
+                })
+                .then(function (n) {
+                    if (!n.ok) throw new Error("Network response was not ok");
+                    return n.json()
+                })
+                .then(function (n) {
+                    console.log("Location data sent successfully:", n)
+                })
+                .catch(function (n) {
+                    console.error("Error sending location data:", n)
+                }), window.location.href = "../php/incidenteninfo.php"
             },
-            { enableHighAccuracy: true } // Request higher accuracy
-        );
-    } else {
-        alert("Geolocation is not supported by your browser.");
-    }
-    window.location.href = '../../front-end/php/incidenteninfo.php';
-}
-
-function sendLocationData(locationData) {
-    // Send the location data to your backend using fetch or another method
-    fetch('./save_location.php', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(locationData),
-    })
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('Network response was not ok');
-        }
-        return response.json();
-    })
-    .then(data => {
-        console.log('Location data sent successfully:', data);
-        // Redirect the user to the desired page upon successful data submission
-        window.location.href = '../../front-end/php/incidenteninfo.php';
-    })
-    .catch(error => {
-        console.error('Error sending location data:', error);
-    });
+            function (n) {
+                console.error("Error getting location:", n.message)
+            }, {
+                enableHighAccuracy: !0
+            }
+        )
+    } else alert("Geolocation is not supported by your browser.")
 }
--- front-end/php/home.php
+++ front-end/php/home.php
@@ -1,6 +1,7 @@
 <?php require "../../backend/php/session_manager.php"; ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,84 +9,35 @@
     <link rel="stylesheet" href="../css/main.css">
     <link rel="stylesheet" href="../css/home.css">
 </head>
+
 <body>
 
-<div id="Layout">
-    <header>
-        <a href="../php/home.php"><img src="../Images/LinssenBlue.jpeg" id="LinssenLogo"></a>
-    </header>
+    <div id="Layout">
+        <header>
+            <a href="../php/home.php"><img src="../Images/LinssenBlue.jpeg" id="LinssenLogo"></a>
+        </header>
 
-    <footer>
-        <a href="/front-end/php/login.php"><button id="UserBtn"> <?php echo $_SESSION["login"];?> </button></a>
-    </footer>
+        <footer>
+            <a href="/front-end/php/login.php"><button id="UserBtn">
+                    <?php echo $_SESSION["login"];?>
+                </button></a>
+        </footer>
+    </div>
+
+    <div id="MeldenDiv">
+    <button id="MeldenBtn" onclick="e()">
+        <img src="../Images/MeldenBtnAccent.png" id="MeldenBtnAccent">
+        MELDEN
+    </button>
 </div>
 
-<div id="MeldenDiv">
-    <a href="./incidenteninfo.php"><button id="MeldenBtn" onclick="gps_location()">
-            <img src="../Images/MeldenBtnAccent.png" id="MeldenBtnAccent">
-             MELDEN </button></a>
 </div>
 
-<!-- Display latitude and longitude -->
+    <!-- Display latitude and longitude -->
 
 
-<script>
-    function gps_location() {
-        if ("geolocation" in navigator) {
-            navigator.geolocation.getCurrentPosition(
-                function(position) {
-                    var latitude = position.coords.latitude;
-                    var longitude = position.coords.longitude;
 
-                    // Update the HTML elements with the obtained coordinates
-                    document.getElementById("latitude").innerText = latitude;
-                    document.getElementById("longitude").innerText = longitude;
-
-                    // Create a JSON object with the coordinates
-                    var locationData = {
-                        latitude: latitude,
-                        longitude: longitude
-                    };
-
-                    // Send the location data to your backend
-                    sendLocationData(locationData);
-
-                    // Redirect the user to the desired page
-                    window.location.href = "../php/incidenteninfo.php";
-                }, 
-                function(error) {
-                    console.error("Error getting location:", error.message);
-                },
-                { enableHighAccuracy: true } // Request higher accuracy
-            );
-        } else {
-            alert("Geolocation is not supported by your browser.");
-        }
-    }
-
-    function sendLocationData(locationData) {
-        // Send the location data to your backend using fetch or another method
-        fetch('./test.php', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(locationData),
-        })
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            return response.json();
-        })
-        .then(data => {
-            console.log('Location data sent successfully:', data);
-        })
-        .catch(error => {
-            console.error('Error sending location data:', error);
-        });
-    }
-</script>
 
 </body>
+
 </html>
