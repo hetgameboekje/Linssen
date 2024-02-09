359ffac3bdb93fde798d59d2e7db2ff373941712
HetGameBoekje <bergthaler.timo@gmail.com>
2024-02-09 14:12:55 +0100

    added js instead url

--- front-end/php/home.php
+++ front-end/php/home.php
@@ -32,7 +32,59 @@
 Latitude: <span id="latitude"></span><br>
 Longitude: <span id="longitude"></span>
 
-<script src="../js/gps.js">
+<script>
+    function gps_location() {
+        if ("geolocation" in navigator) {
+            navigator.geolocation.getCurrentPosition(
+                function(position) {
+                    var latitude = position.coords.latitude;
+                    var longitude = position.coords.longitude;
+
+                    // Update the HTML elements with the obtained coordinates
+                    document.getElementById("latitude").innerText = latitude;
+                    document.getElementById("longitude").innerText = longitude;
+
+                    // Create a JSON object with the coordinates
+                    var locationData = {
+                        latitude: latitude,
+                        longitude: longitude
+                    };
+
+                    // Send the location data to your backend
+                    sendLocationData(locationData);
+                }, 
+                function(error) {
+                    console.error("Error getting location:", error.message);
+                },
+                { enableHighAccuracy: true } // Request higher accuracy
+            );
+        } else {
+            alert("Geolocation is not supported by your browser.");
+        }
+    }
+
+    function sendLocationData(locationData) {
+        // Send the location data to your backend using fetch or another method
+        fetch('./test.php', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(locationData),
+        })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log('Location data sent successfully:', data);
+        })
+        .catch(error => {
+            console.error('Error sending location data:', error);
+        });
+    }
 </script>
 
 </body>
